0597119a7744eac027638423526290e79df54d4f
dyan <dyan@ubuntu-crappy.home>
2019-04-29 16:26:34 +1000

    stuff n things

--- v2/background.js
+++ v2/background.js
@@ -1,11 +1,31 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-});
+/*chrome.runtime.onInstalled.addListener(function() {
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+		chrome.declarativeContent.onPageChanged.addRules([{
+			conditions: [new chrome.declarativeContent.PageStateMatcher({
+				pageUrl: {hostEquals: 'developer.chrome.com'},
+      			})],
+			actions: [new chrome.declarativeContent.ShowPageAction()]
+		}]);
+	});
+});*/
+
+/*document.addEventListener("DOMContentLoaded", function() {
+	chrome.tabs.query({active:true,currentWindow:true}, function(tabs){
+		console.log(tabs[0].url);
+	});
+});*/
+
+/*chrome.runtime.onInstalled.addListener(function(){
+	chrome.addEventListener("DOMContentLoaded", function(){
+		chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+			console.log(tabs[0].url);
+		});
+	);
+});*/
+
+chrome.tabs.onUpdated.addListener(function(){
+	chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+		return tabs[0].url;
+	});
+	
 });
--- v2/popup.js
+++ v2/popup.js
@@ -1,3 +1,13 @@
-console.log('hello');
-console.log(chrome.tabs.Tab.url);
-document.getElementById("siteUrl").value = chrome.tabs.getCurrent(function(){});
+function getUrl(){
+	chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+		return tabs[0].url;
+	});
+};
+
+chrome.tabs.getCurrent(function(currentTab){
+	console.log(currentTab.url);
+});
+
+/*console.log('hello');
+console.log(getUrl());
+document.getElementById("siteUrl").value = chrome.tabs.getCurrent(function(){});*/

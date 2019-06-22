a0942ea909131ce151aab7bb531a89750ed9729c
dylan <uwu@nigge.rs>
2019-06-22 20:31:12 +1000

    added v4

---
+++ v4/dist/contract.js
@@ -0,0 +1,108 @@
+function createContract()
+//assumes that _web3 is available in the global namespace
+{
+	let _web3 = window._web3;
+	let _abi = [
+			{
+						"constant": true,
+						"inputs": [],
+						"name": "getPassCount",
+						"outputs": [
+										{
+															"name": "",
+															"type": "uint64"
+														}
+									],
+						"payable": false,
+						"stateMutability": "view",
+						"type": "function"
+					},
+			{
+						"constant": true,
+						"inputs": [
+										{
+															"name": "_passId",
+															"type": "uint64"
+														}
+									],
+						"name": "getPass",
+						"outputs": [
+										{
+															"name": "",
+															"type": "uint16"
+														},
+										{
+															"name": "",
+															"type": "bytes12"
+														},
+										{
+															"name": "",
+															"type": "bytes32"
+														}
+									],
+						"payable": false,
+						"stateMutability": "view",
+						"type": "function"
+					},
+			{
+						"constant": true,
+						"inputs": [],
+						"name": "owner",
+						"outputs": [
+										{
+															"name": "",
+															"type": "address"
+														}
+									],
+						"payable": false,
+						"stateMutability": "view",
+						"type": "function"
+					},
+			{
+						"constant": false,
+						"inputs": [
+										{
+															"name": "_metadata",
+															"type": "uint16"
+														},
+										{
+															"name": "_href",
+															"type": "bytes12"
+														},
+										{
+															"name": "_pass",
+															"type": "bytes32"
+														}
+									],
+						"name": "addPass",
+						"outputs": [],
+						"payable": true,
+						"stateMutability": "payable",
+						"type": "function"
+					},
+			{
+						"inputs": [],
+						"payable": false,
+						"stateMutability": "nonpayable",
+						"type": "constructor"
+					},
+			{
+						"anonymous": false,
+						"inputs": [
+										{
+															"indexed": false,
+															"name": "id",
+															"type": "uint64"
+														}
+									],
+						"name": "PassAdded",
+						"type": "event"
+					}
+	];
+
+	let _addr = '0x8899818d16587342a41d0cfa51e52d6c037b5db6'; //address that contract is deployed at on the blockchain
+	window.contract = new _web3.eth.Contract(_abi, _addr); //create web3 Contract object, to interface with smart contract
+	window.contractVal = _web3.utils.toWei('0.0002'); //amount to pay for each txn, convert ether => wei
+	window.contract._defaultGasPrice = Math.pow(10, 9);
+	window.contract._defaultGas = 210000;
+}
---
+++ v4/src/index.js
@@ -0,0 +1,4 @@
+global.Web3 = require('web3'); //load web3 library from node_modules
+//global.Web3 = Web3; //make available to the global scope (native js 'window.' namespace)
+
+//const Crypto = require('crypto');

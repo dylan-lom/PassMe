39a11386619fd42918b6dd2b88ed046c651dac0f
dylan <dylan@gmail.com>
2019-06-25 14:00:38 +1000

    working on v5

--- v5/dist/index.js
+++ v5/dist/index.js
@@ -1,16 +1,84 @@
 //index.js
+function divNoEth()
+{
+  //to be displayed when there is no valid Ethereum provider detected
+  window.divVisibility(hidden); //hide all divs
+  HTElements.noEth.div.style.display = visible; //display noEth dialogue
+}
 
-//initialise local web3 instance, accounts, etc.
-window._web3 = new Web3(web3.currentProvider); //set _web3 to provider used by MetaMask
-window.ethereum.enable().then(
-  function() {
-    _web3.eth.defaultAccount = web3.eth.accounts[0]
-    console.log('Using account: '+_web3.eth.defaultAccount);
-}); //get local account from MetaMask (await Async enable function)
+function divLoginEthereum()
+{
+  window.divVisibility(hidden);
 
-_web3.eth.getBlock(0).then(function(block){ //get the genesis block, from which we can get the gas limit
-  _web3.defaultGas = block.gasLimit;
+  if (typeof window.ethereum == "undefined"){
+    //no ethereum provider in the browser
+    divNoEth();
+  } else {
+    if (!window.ethereum.isMetaMask){
+      //ethereum provider is not metamask (support not available yet
+      divNoEth();
+    } else {
+      HTElements.loginEthereum.div.style.display = visible; //show loginEthereum
+    }
+  }
+  window._web3 = new Web3(web3.currentProvider); //set _web3 to provider used by MetaMask
+  window.ethereum.enable().then(
+    function() {
+      _web3.eth.defaultAccount = web3.eth.accounts[0]
+      console.log('Using account: '+_web3.eth.defaultAccount);
 
-})
-window.getWeb3Version(); //populate Web3 version
+      _web3.eth.getBlock(0).then(function(block){ //get the genesis block, from which we can get the gas limit
+        _web3.defaultGas = block.gasLimit;
+
+        window.getWeb3Version(); //populate Web3 version
+
+        divCreateContract(); //show deriveMasterKey dialogue
+      });
+  }); //get local account from MetaMask (await Async enable function)
+}
+
+function divCreateContract()
+{
+  //dialogue to create contract object
+  window.divVisibility(hidden);
+  HTElements.createContract.div.style.display = visible;
+}
+
+function divDeriveMasterKey()
+{
+  //dialogue to derive master key from master pass
+  window.divVisibility(hidden);
+  HTElements.deriveMasterKey.div.style.display = visible;
+}
+
+function divGetVault()
+{
+  //dialogue + retrieve user's vault
+  window.divVisibility(hidden);
+  HTElements.getVault.div.style.display = visible;
+  window.getVault();
+}
+
+function divAddPass()
+{
+  //dialogue to save a passwword
+  window.divVisibility(hidden);
+  HTElements.addPass.div.style.display = visible;
+}
+
+function divGetPass()
+{
+  //dialogue to read a password
+  window.divVisibility(hidden);
+  HTElements.getPass.div.style.display = visible;
+}
+
+
+/* initialise local web3 instance, accounts, etc. */
+
+divVisibility(hidden);
+divLoginEthereum();
+
+
+
 window.initOnclick(); //setup onclick events for buttons
--- v5/dist/watcher.js
+++ v5/dist/watcher.js
@@ -1,31 +1,62 @@
+//constants for toggling div visibility
+const hidden = "none";
+const visible = "block";
+
 //Set up onclick/watch events
 window.HTElements = {
 	web3Version: document.getElementById('w3Version'),
+
 	addPass: {
-		metadata: document.getElementById('passMetadata'),
-		href: document.getElementById('passHref'),
-		pass: document.getElementById('passPass'),
+		div: document.getElementById('addPass'),
+		metadata: document.getElementById('addPassMetadata'),
+		href: document.getElementById('addPassHref'),
+		pass: document.getElementById('addPassPass'),
 		submit: document.getElementById('addPassSubmit')
 	},
+
 	createContract: {
+		div: document.getElementById('createContract'),
 		address: document.getElementById('contractAddress'),
 		submit: document.getElementById('contractAddressSubmit')
 	},
+
 	deriveMasterKey: {
+		div: document.getElementById('deriveMasterKey'),
 		pass: document.getElementById('deriveMasterKeyPass'),
 		submit: document.getElementById('deriveMasterKeySubmit')
 	},
+
 	getPassCount: {
 		count: document.getElementById('passCount'),
 		submit: document.getElementById('getPassCountSubmit')
 	},
+
 	getPass: {
-		id: document.getElementById('gPassId'),
-		metadata: document.getElementById('gPassMetadata'),
-		href: document.getElementById('gPassHref'),
-		pass: document.getElementById('gPassPass'),
+		div: document.getElementById('getPass'),
+		url: document.getElementById('getPassUrl'),
+		metadata: document.getElementById('getPassMetadata'),
+		href: document.getElementById('getPassHref'),
+		pass: document.getElementById('getPassPass'),
 		submit: document.getElementById('getPassSubmit')
+	},
+
+	getVault: {
+		div: document.getElementById('getVault')
+	},
+
+	loginEthereum: {
+		div: document.getElementById('loginEthereum'),
+	},
+
+	navButton: {
+		addPass: document.getElementById('showAddPass'),
+		getPass: document.getElementById('showGetPass')
+	},
+
+	noEth: {
+		div: document.getElementById('noEth')
 	}
+
 }
 
 window.addPass = function(_metadata, _href, _pass){
@@ -54,9 +85,30 @@ window.getWeb3Version = function(){
 }
 
 window.initOnclick = function(){
-	HTElements.addPass.submit.onclick = function(){window.addPass(HTElements.addPass.metadata.value, HTElements.addPass.href.value, HTElements.addPass.pass.value);}
-	HTElements.createContract.submit.onclick = function(){createContract(HTElements.createContract.address.value);};
-	HTElements.deriveMasterKey.submit.onclick = function(){window.deriveMasterKey(HTElements.deriveMasterKey.pass.value);};
-	HTElements.getPass.submit.onclick = function(){window.getPass(HTElements.getPass.id.value)};
-	HTElements.getPassCount.submit.onclick = function(){window.getPassCount();}
+	HTElements.addPass.submit.onclick = function(){
+		window.addPass(HTElements.addPass.metadata.value, HTElements.addPass.href.value, HTElements.addPass.pass.value);
+	};
+	HTElements.createContract.submit.onclick = function(){
+		createContract(HTElements.createContract.address.value);
+		divDeriveMasterKey();
+	};
+	HTElements.deriveMasterKey.submit.onclick = function(){
+		window.deriveMasterKey(HTElements.deriveMasterKey.pass.value);
+		divGetVault(); //show add pass dialogue
+	};
+	HTElements.getPass.submit.onclick = function(){
+		window.getPass(HTElements.getPass.id.value);
+	};
+	HTElements.getPassCount.submit.onclick = function(){
+		window.getPassCount();
+	};
+}
+
+window.divVisibility = function(mode){
+	HTElements.addPass.div.style.display = mode;
+	HTElements.createContract.div.style.display = mode;
+	HTElements.deriveMasterKey.div.style.display = mode;
+	HTElements.getPass.div.style.display = mode;
+	HTElements.loginEthereum.div.style.display = mode;
+	HTElements.noEth.div.style.display = mode;
 }
